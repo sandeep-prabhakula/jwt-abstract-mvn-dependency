a621771e469aadbd9d86947ae74aae3597383d55
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2025-07-30 22:24:36 +0530

    catching exceptions when expiration is set to 0

--- src/main/java/com/sandeepprabhakula/TokenGenerator.java
+++ src/main/java/com/sandeepprabhakula/TokenGenerator.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.Key;
 import java.util.Date;
@@ -12,47 +14,90 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class TokenGenerator {
+    private final Logger log = LoggerFactory.getLogger(TokenGenerator.class);
 
-    public String generateToken(String SECRET,String userName,Map<String,Object>claims,long expirationDuration){
+    public String generateToken(String SECRET, String userName, Map<String, Object> claims, long expirationDuration) {
+        try {
+            return createToken(SECRET, claims, userName, expirationDuration);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
 
-        return createToken(SECRET,claims,userName,expirationDuration);
     }
 
-    private String createToken(String SECRET, Map<String, Object> claims, String userName,long expiration) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(userName)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+expiration))
-                .signWith(getSignKey(SECRET), SignatureAlgorithm.HS256)
-                .compact();
+    private String createToken(String SECRET, Map<String, Object> claims, String userName, long expiration) {
+        try {
+            expiration = expiration != 0 ? expiration : 1000 * 60 * 30;
+            log.info(String.valueOf(expiration));
+            return Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(userName)
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                    .signWith(getSignKey(SECRET), SignatureAlgorithm.HS256)
+                    .compact();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException();
+        }
+
     }
 
     private Key getSignKey(String SECRET) {
-        byte[]keyBytes = Decoders.BASE64URL.decode(SECRET);
-        return Keys.hmacShaKeyFor(keyBytes);
+        try {
+            byte[] keyBytes = Decoders.BASE64URL.decode(SECRET);
+            return Keys.hmacShaKeyFor(keyBytes);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
     }
 
 
-    public Date extractExpiration(String SECRET,String token) {
-        return extractClaim(SECRET,token, Claims::getExpiration);
+    public Date extractExpiration(String SECRET, String token) {
+        try {
+            return extractClaim(SECRET, token, Claims::getExpiration);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
     }
 
     private <T> T extractClaim(String SECRET, String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(SECRET, token);
-        return claimsResolver.apply(claims);
+        try {
+
+            final Claims claims = extractAllClaims(SECRET, token);
+            return claimsResolver.apply(claims);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
-    public Claims extractAllClaims(String SECRET,String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignKey(SECRET))
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+    public Claims extractAllClaims(String SECRET, String token) {
+        try {
+            return Jwts
+                    .parserBuilder()
+                    .setSigningKey(getSignKey(SECRET))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
     }
 
-    private Boolean isTokenExpired(String SECRET,String token) {
-        return extractExpiration(SECRET,token).before(new Date());
+    private Boolean isTokenExpired(String SECRET, String token) {
+        try {
+            return extractExpiration(SECRET, token).before(new Date());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 }
